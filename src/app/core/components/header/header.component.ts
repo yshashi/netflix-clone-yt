62a7eac21ab8a053edb963023aa63de8958b6a33
Sashikumar <yshashi30@gmail.com>
2023-12-12 19:25:00 +0800

    chore: added signout logic

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   @Input({required: true}) userImg: string = '';
+  username = JSON.parse(sessionStorage.getItem('loggedInUser')!).name;
+  auth = inject(AuthService);
   navList = ["Home", "TV Shows", "News & Popular", "My List", "Browse by Language"]
 }
